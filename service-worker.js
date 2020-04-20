81b33bfb6073dfb35b5ea6c4ede4932ffc9def82
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-20 15:18:58 +0000

    Deploy ieeemysight4rehab/ieeemysight4rehab.github.io to github.com/ieeemysight4rehab/ieeemysight4rehab.github.io.git:master

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.c1bb7711252ec35dd69e59f107a14628.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.eadc13c1bfb7089c3c36a064f17d2b53.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
