2d986178c28daed54a07493f26363bd3f42fad9b
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-10 02:08:48 +0000

    Deploy ieeemysight4rehab/ieeemysight4rehab.github.io to github.com/ieeemysight4rehab/ieeemysight4rehab.github.io.git:master

--- precache-manifest.90c900191d25ab17e101c2e28f1c51b7.js
+++ precache-manifest.90c900191d25ab17e101c2e28f1c51b7.js
@@ -104,10 +104,6 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-Thin.ad538a69.woff2"
   },
   {
-    "revision": "d3b47375afd904983d9be8d6e239a949",
-    "url": "/fonts/Roboto-Thin.d3b47375.woff"
-  },
-  {
     "revision": "5b4a33e176ff736a74f0ca2dd9e6b396",
     "url": "/fonts/Roboto-ThinItalic.5b4a33e1.woff2"
   },
@@ -124,8 +120,8 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-Light.c73eb1ce.woff"
   },
   {
-    "revision": "e8eaae902c3a4dacb9a5062667e10576",
-    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
+    "revision": "d3b47375afd904983d9be8d6e239a949",
+    "url": "/fonts/Roboto-Thin.d3b47375.woff"
   },
   {
     "revision": "13efe6cbc10b97144a28310ebdeda594",
@@ -134,10 +130,6 @@ self.__precacheManifest = [
   {
     "revision": "73f0a88bbca1bec19fb1303c689d04c6",
     "url": "/fonts/Roboto-Regular.73f0a88b.woff2"
-  },
-  {
-    "revision": "35b07eb2f8711ae08d1f58c043880930",
-    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
   },
   {
     "revision": "4357beb823a5f8d65c260f045d9e019a",
@@ -194,6 +186,14 @@ self.__precacheManifest = [
   {
     "revision": "cc2fadc3928f2f223418887111947b40",
     "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "35b07eb2f8711ae08d1f58c043880930",
+    "url": "/fonts/Roboto-Regular.35b07eb2.woff"
+  },
+  {
+    "revision": "e8eaae902c3a4dacb9a5062667e10576",
+    "url": "/fonts/Roboto-LightItalic.e8eaae90.woff2"
   },
   {
     "revision": "9f75886c99f7ad70d485d9e39895c62a",
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.5853a5e8cb9d0f939741197756cd7a83.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.90c900191d25ab17e101c2e28f1c51b7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
