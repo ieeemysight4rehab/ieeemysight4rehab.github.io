6a23c7b2d7c24f0f78073b460c40a256e620f6da
Jerry Chong <jerrychong25@gmail.com>
2020-07-15 20:45:34 +0800

    Moved Template Partner to Footer
    -Removed Template Partner in partners.vue
    -Added Template By GDG Jalandhar in footer.json (#43)

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -55,56 +55,6 @@
                     </v-flex>
                 </v-layout>
             </v-flex>
-
-            <v-flex xs12 md12 class="mt-3">
-                <p class="google-font my-2" style="font-size:110%">Template Partner</p>
-
-                <v-layout class="transparent" >
-                    <v-flex xs12 sm11 class="elevation-0 transparent">
-                        <v-card class="elevation-0 pa-0 ma-0 transparent">
-                            <v-container grid-list-sm fluid class="pa-0 ma-0">
-                                <v-layout row wrap class="ma-0 pa-0">
-                                    <v-flex
-                                        xs6
-                                        md2
-                                        sm4
-                                        d-flex
-                                        class="white elevation-0">
-                                    <v-card flat tile class="d-flex pa-2 ma-1 elevation-1" style="border-radius:5px;border-width: 1px; border-style: solid;">
-                                        <a href="https://gdgjalandhar.com" target="_blank">
-                                        <v-tooltip bottom>
-                                            <template v-slot:activator="{ on }">
-                                        <v-img
-                                            :src="require('@/assets/img/partners/gdgjalandhar.png')"
-                                            :lazy-src="require('@/assets/img/partners/gdgjalandhar.png')"
-                                            aspect-ratio="3.0"
-                                            class="white"
-                                            v-on="on"
-                                        >
-                                            <v-layout
-                                                slot="placeholder"
-                                                fill-height
-                                                align-center
-                                                justify-center
-                                                ma-0
-                                            >
-                                                <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-                                            </v-layout>
-                                        </v-img>
-                                        </template>
-                                        <span class="google-font">GDG Jalandhar</span>
-                                        </v-tooltip>
-                                        </a>
-                                    </v-card>
-                                    </v-flex>
-
-                                </v-layout>
-                            </v-container>
-                        </v-card>
-                    </v-flex>
-                </v-layout>
-
-            </v-flex>
         </v-layout>
     </v-container>
 </template>
