a95820f11b5f577ef8bee9a54e9841fc2991ed3d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-18 15:51:55 +0000

    Deploy ieeemysight4rehab/ieeemysight4rehab.github.io to github.com/ieeemysight4rehab/ieeemysight4rehab.github.io.git:master

--- precache-manifest.b3a83bc9ce6a5d7623e5f0dd7c07a0c6.js
+++ precache-manifest.b3a83bc9ce6a5d7623e5f0dd7c07a0c6.js
@@ -224,12 +224,12 @@ self.__precacheManifest = [
     "url": "/index.html"
   },
   {
-    "revision": "0817d69c5198a01f2555961c2bb2e48f",
-    "url": "/apple-touch-icon.png"
-  },
-  {
     "revision": "061f63e8fbf1d2fe82cd8bf4d46f0a0c",
     "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "0817d69c5198a01f2555961c2bb2e48f",
+    "url": "/apple-touch-icon.png"
   },
   {
     "revision": "6b60e395572c2b5bfec12bae48706965",
@@ -264,12 +264,20 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-12-1.jpg"
   },
   {
+    "revision": "0f423922ef63a2b829c24d67b51f11ac",
+    "url": "/img/projects/events/event-12-3.jpg"
+  },
+  {
     "revision": "d2091d40c5b66709dd613d345f000cb3",
     "url": "/img/projects/events/event-12-2.jpg"
   },
   {
     "revision": "30eb5c0406669c7b272adab37e4f93f0",
     "url": "/img/projects/events/event-12-5.jpg"
+  },
+  {
+    "revision": "174351a3972d92525ac78c3ec8c08d80",
+    "url": "/img/projects/events/event-2-1.png"
   },
   {
     "revision": "2e5d3316079d060aa8693e97fb890d36",
@@ -284,28 +292,20 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-2-4.jpg"
   },
   {
-    "revision": "174351a3972d92525ac78c3ec8c08d80",
-    "url": "/img/projects/events/event-2-1.png"
-  },
-  {
-    "revision": "e3a9c489886dbb837a32799014df1520",
-    "url": "/img/projects/events/event-2-5.jpg"
-  },
-  {
-    "revision": "5fce29daf242af3258bb6b85a007dc43",
-    "url": "/img/projects/events/event-2-6.jpg"
+    "revision": "8fa676f110108ea41991e2aafe9b9fbf",
+    "url": "/img/projects/events/event-2-7.jpg"
   },
   {
     "revision": "348bed685527bac23c15940ca221fb85",
     "url": "/img/projects/events/event-2-8.jpg"
   },
   {
-    "revision": "8fa676f110108ea41991e2aafe9b9fbf",
-    "url": "/img/projects/events/event-2-7.jpg"
-  },
-  {
     "revision": "151a9099beade2b871cf26a94fff8999",
     "url": "/img/projects/events/event-2-9.jpg"
+  },
+  {
+    "revision": "5fce29daf242af3258bb6b85a007dc43",
+    "url": "/img/projects/events/event-2-6.jpg"
   },
   {
     "revision": "bbd4ced9cbfa7240885f60f7fd26358f",
@@ -316,10 +316,6 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-5.png"
   },
   {
-    "revision": "022bb3a2ad54e94ff296556a824124c0",
-    "url": "/img/projects/events/event-7.png"
-  },
-  {
     "revision": "41975a8d62de8de41a4ce1ade77f1ffc",
     "url": "/img/projects/events/event-8.png"
   },
@@ -328,20 +324,24 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-9-2.png"
   },
   {
-    "revision": "af90bd193aaed1ab3b7564f5a9eab953",
-    "url": "/img/projects/events/event-9-4.png"
-  },
-  {
     "revision": "4e540cbf5064e1a83a38f87362d3ddae",
     "url": "/img/projects/events/event-9-3.png"
   },
   {
-    "revision": "92bab103a4776ea027b9836ffc8f9b05",
-    "url": "/img/projects/prototype/prototype-3.png"
+    "revision": "af90bd193aaed1ab3b7564f5a9eab953",
+    "url": "/img/projects/events/event-9-4.png"
+  },
+  {
+    "revision": "af693a762e320d975fe4780b95a50364",
+    "url": "/img/projects/prototype/prototype-1.png"
   },
   {
     "revision": "aa8109c5e9a2a793b97cdf820ee1877b",
     "url": "/img/projects/prototype/prototype-2.png"
+  },
+  {
+    "revision": "92bab103a4776ea027b9836ffc8f9b05",
+    "url": "/img/projects/prototype/prototype-3.png"
   },
   {
     "revision": "a2621ed6bf38f1cec94ffdc9b0845fbe",
@@ -352,12 +352,12 @@ self.__precacheManifest = [
     "url": "/img/projects/prototype/prototype-5.png"
   },
   {
-    "revision": "0f423922ef63a2b829c24d67b51f11ac",
-    "url": "/img/projects/events/event-12-3.jpg"
-  },
-  {
     "revision": "2167327c868d5f59a76911c4e0ae46b9",
     "url": "/img/projects/prototype/prototype-6.png"
+  },
+  {
+    "revision": "abbc8180c1cf9aefdc0bbfdd131e1744",
+    "url": "/img/projects/results/result-1-1.jpg"
   },
   {
     "revision": "b8d808a40abb8a1191d06989b5aaf9cd",
@@ -368,40 +368,32 @@ self.__precacheManifest = [
     "url": "/robots.txt"
   },
   {
-    "revision": "6abd090936f5d516ec245653cc694834",
-    "url": "/img/projects/results/result-1-2.png"
-  },
-  {
-    "revision": "abbc8180c1cf9aefdc0bbfdd131e1744",
-    "url": "/img/projects/results/result-1-1.jpg"
+    "revision": "02744c67a725024d9a966fe3e26c6347",
+    "url": "/img/projects/events/event-10-2.png"
   },
   {
     "revision": "98edc5fc3122147ce23aad0632e51412",
     "url": "/img/projects/events/event-11-1.png"
   },
   {
-    "revision": "c40415ab7acdf3e7b95bf3f3d87fb19a",
-    "url": "/img/projects/events/event-4.png"
+    "revision": "0da4759d5631d1f3b0d08bf9fbaf3163",
+    "url": "/img/projects/events/event-11-2.png"
+  },
+  {
+    "revision": "6abd090936f5d516ec245653cc694834",
+    "url": "/img/projects/results/result-1-2.png"
+  },
+  {
+    "revision": "022bb3a2ad54e94ff296556a824124c0",
+    "url": "/img/projects/events/event-7.png"
   },
   {
     "revision": "363dfd538079599dc550264a1ff37366",
     "url": "/img/projects/events/event-6.png"
   },
   {
-    "revision": "af693a762e320d975fe4780b95a50364",
-    "url": "/img/projects/prototype/prototype-1.png"
-  },
-  {
-    "revision": "0da4759d5631d1f3b0d08bf9fbaf3163",
-    "url": "/img/projects/events/event-11-2.png"
-  },
-  {
     "revision": "99aa7d27ac285d89aca7077ec0576d9a",
     "url": "/img/projects/prototype/prototype-7.png"
-  },
-  {
-    "revision": "8785ae4c0b9ee241c8e75c18dbf01b09",
-    "url": "/img/projects/prototype/prototype-8-1.png"
   },
   {
     "revision": "6cfafa6f018e5eaaca4ce12000f9ae0a",
@@ -412,23 +404,31 @@ self.__precacheManifest = [
     "url": "/img/projects/prototype/prototype-9.png"
   },
   {
-    "revision": "02744c67a725024d9a966fe3e26c6347",
-    "url": "/img/projects/events/event-10-2.png"
+    "revision": "8785ae4c0b9ee241c8e75c18dbf01b09",
+    "url": "/img/projects/prototype/prototype-8-1.png"
   },
   {
-    "revision": "4bb694ac4c54643e3e7fb7d98a272c80",
-    "url": "/img/projects/events/event-9-1.png"
+    "revision": "c40415ab7acdf3e7b95bf3f3d87fb19a",
+    "url": "/img/projects/events/event-4.png"
+  },
+  {
+    "revision": "e3a9c489886dbb837a32799014df1520",
+    "url": "/img/projects/events/event-2-5.jpg"
   },
   {
     "revision": "f05b324b96c0876fc31f8ec9552d4e92",
     "url": "/img/projects/prototype/prototype-10.png"
   },
   {
-    "revision": "d9818446c232c9656e00614c66483805",
-    "url": "/img/projects/events/event-9-5.png"
+    "revision": "4bb694ac4c54643e3e7fb7d98a272c80",
+    "url": "/img/projects/events/event-9-1.png"
   },
   {
     "revision": "5896ff1ea49c4e0b7a52b63eec1842e6",
     "url": "/img/projects/events/event-12-4.png"
+  },
+  {
+    "revision": "d9818446c232c9656e00614c66483805",
+    "url": "/img/projects/events/event-9-5.png"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.87afe8f260c7dc123a592ae25c53541a.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.b3a83bc9ce6a5d7623e5f0dd7c07a0c6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
