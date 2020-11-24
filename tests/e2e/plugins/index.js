41386098394b017bf331010930b443b498b608b4
Jerry Chong <jerrychong25@gmail.com>
2020-11-24 10:53:10 +0800

    Modified Cypress plugin file
    -Added fix for 404 Not Found during CI

--- tests/e2e/plugins/index.js
+++ tests/e2e/plugins/index.js
@@ -10,12 +10,21 @@
 // const webpack = require('@cypress/webpack-preprocessor')
 
 module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
+  
   // on('file:preprocessor', webpack({
   //  webpackOptions: require('@vue/cli-service/webpack.config'),
   //  watchOptions: {}
   // }))
 
-  require('@cypress/code-coverage/task')(on, config)
+  on('before:browser:launch', (browser = {}, args) => {
+    if (browser.name === 'chrome') { 
+      // ^ make sure this is your browser name, you may 
+      // be using 'canary' or 'chromium' for example, so change it to match!
+      args.push('--proxy-bypass-list=<-loopback>')
+      return args
+    }
+  })
 
   return Object.assign({}, config, {
     fixturesFolder: 'tests/e2e/fixtures',
