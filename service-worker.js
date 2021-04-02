fea7f6834aa8c5e910c09f9ade910773ff023638
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2021-04-02 03:06:42 +0000

    Deploying to master from staging @ 111672b74a8f2478fce949b18854887033ffa6d9 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.7f82a4c45152cbdfe517f9e42c7428ef.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.fd6b9f306a43e75a0deef57744a2ab36.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
