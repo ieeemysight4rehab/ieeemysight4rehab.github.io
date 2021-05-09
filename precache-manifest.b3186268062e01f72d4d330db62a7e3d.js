e105f4b36eac07cfa44ec06640aa60b60f6970cb
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2021-05-09 09:29:21 +0000

    Deploying to master from staging @ fd9ea55d0936f49d74c834c659b5f37a7184d983 🚀

--- precache-manifest.b3186268062e01f72d4d330db62a7e3d.js
+++ precache-manifest.b3186268062e01f72d4d330db62a7e3d.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "456166ed876b605b949c",
+    "revision": "fa562d0f81a858c63cdb",
     "url": "/css/app.f4e27866.css"
   },
   {
-    "revision": "456166ed876b605b949c",
-    "url": "/js/app.a966d1ad.js"
+    "revision": "fa562d0f81a858c63cdb",
+    "url": "/js/app.7f98cf2f.js"
   },
   {
     "revision": "b4a80f7eadbe600707d1",
@@ -252,10 +252,6 @@ self.__precacheManifest = [
     "url": "/fonts/Roboto-BoldItalic.4fe0f73c.woff"
   },
   {
-    "revision": "59eb3601394dd87f30f82433fb39dd94",
-    "url": "/fonts/Roboto-Black.59eb3601.woff2"
-  },
-  {
     "revision": "313a65630d341645c13e4f2a0364381d",
     "url": "/fonts/Roboto-Black.313a6563.woff"
   },
@@ -266,6 +262,10 @@ self.__precacheManifest = [
   {
     "revision": "cc2fadc3928f2f223418887111947b40",
     "url": "/fonts/Roboto-BlackItalic.cc2fadc3.woff"
+  },
+  {
+    "revision": "59eb3601394dd87f30f82433fb39dd94",
+    "url": "/fonts/Roboto-Black.59eb3601.woff2"
   },
   {
     "revision": "3fafe714cfc3a1c4891addd0e22720b0",
@@ -284,7 +284,7 @@ self.__precacheManifest = [
     "url": "/img/firebase.1c973c35.svg"
   },
   {
-    "revision": "0cdc28ebbaf587cc070fb833b4e58d4d",
+    "revision": "cd16e140598ec1a53eaf0e70c115427a",
     "url": "/index.html"
   },
   {
@@ -384,10 +384,6 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-5.png"
   },
   {
-    "revision": "022bb3a2ad54e94ff296556a824124c0",
-    "url": "/img/projects/events/event-7.png"
-  },
-  {
     "revision": "41975a8d62de8de41a4ce1ade77f1ffc",
     "url": "/img/projects/events/event-8.png"
   },
@@ -456,8 +452,16 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-11-2.png"
   },
   {
+    "revision": "c40415ab7acdf3e7b95bf3f3d87fb19a",
+    "url": "/img/projects/events/event-4.png"
+  },
+  {
     "revision": "363dfd538079599dc550264a1ff37366",
     "url": "/img/projects/events/event-6.png"
+  },
+  {
+    "revision": "f05b324b96c0876fc31f8ec9552d4e92",
+    "url": "/img/projects/prototype/prototype-10.png"
   },
   {
     "revision": "99aa7d27ac285d89aca7077ec0576d9a",
@@ -468,31 +472,27 @@ self.__precacheManifest = [
     "url": "/img/projects/prototype/prototype-8-1.png"
   },
   {
-    "revision": "6cfafa6f018e5eaaca4ce12000f9ae0a",
-    "url": "/img/projects/prototype/prototype-8-2.png"
-  },
-  {
     "revision": "0cc20aa8ab1da033f0ca03747c55a590",
     "url": "/img/projects/prototype/prototype-9.png"
   },
   {
-    "revision": "f05b324b96c0876fc31f8ec9552d4e92",
-    "url": "/img/projects/prototype/prototype-10.png"
+    "revision": "022bb3a2ad54e94ff296556a824124c0",
+    "url": "/img/projects/events/event-7.png"
   },
   {
     "revision": "4bb694ac4c54643e3e7fb7d98a272c80",
     "url": "/img/projects/events/event-9-1.png"
   },
   {
-    "revision": "c40415ab7acdf3e7b95bf3f3d87fb19a",
-    "url": "/img/projects/events/event-4.png"
+    "revision": "d9818446c232c9656e00614c66483805",
+    "url": "/img/projects/events/event-9-5.png"
+  },
+  {
+    "revision": "6cfafa6f018e5eaaca4ce12000f9ae0a",
+    "url": "/img/projects/prototype/prototype-8-2.png"
   },
   {
     "revision": "5896ff1ea49c4e0b7a52b63eec1842e6",
     "url": "/img/projects/events/event-12-4.png"
-  },
-  {
-    "revision": "d9818446c232c9656e00614c66483805",
-    "url": "/img/projects/events/event-9-5.png"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.052198466deb5c24ae7192b5db6ccdb4.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.b3186268062e01f72d4d330db62a7e3d.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
