7f7eec1784d99980a84fca89f5836b22bd40e350
jerrychong25 <15714095+jerrychong25@users.noreply.github.com>
2022-03-19 16:36:59 +0000

    Deploying to master from staging @ 00c4410203dce14abc9bf0af779827c60ebd74cc 🚀

--- precache-manifest.a0866952984d344103aa2ba5e67ad2b0.js
+++ precache-manifest.a0866952984d344103aa2ba5e67ad2b0.js
@@ -388,6 +388,10 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-2-5.jpg"
   },
   {
+    "revision": "19a84db5bc39acf9b472842fc0dd78da",
+    "url": "/img/projects/events/event-2-6.jpg"
+  },
+  {
     "revision": "773f595d171bbd7f2cdf59fd5d469d6a",
     "url": "/img/projects/events/event-2-7.jpg"
   },
@@ -398,10 +402,6 @@ self.__precacheManifest = [
   {
     "revision": "8c9e46efaeb15b8ace0cdf05efd80e8d",
     "url": "/img/projects/events/event-2-9.jpg"
-  },
-  {
-    "revision": "19a84db5bc39acf9b472842fc0dd78da",
-    "url": "/img/projects/events/event-2-6.jpg"
   },
   {
     "revision": "22b0602bdea5861835770f913d5893da",
@@ -428,12 +428,12 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-8.png"
   },
   {
-    "revision": "5176aec4d8715d6d70259500912733f0",
-    "url": "/img/projects/events/event-9-2.png"
-  },
-  {
     "revision": "045a2b1aa97ade9e2134ec869c3592ba",
     "url": "/img/projects/events/event-9-1.png"
+  },
+  {
+    "revision": "5176aec4d8715d6d70259500912733f0",
+    "url": "/img/projects/events/event-9-2.png"
   },
   {
     "revision": "b032766a3ac407de31eab0101a939fd4",
@@ -444,16 +444,16 @@ self.__precacheManifest = [
     "url": "/img/projects/events/event-9-4.png"
   },
   {
+    "revision": "5003a8c083f6a6aedada0865898c6194",
+    "url": "/img/projects/events/event-9-5.png"
+  },
+  {
     "revision": "d3a527417d36ec38c875f224d27b55de",
     "url": "/img/projects/prototype/prototype-1.png"
   },
   {
     "revision": "dfe736895a455046fff8a8d2dd9dbe4b",
     "url": "/img/projects/prototype/prototype-10.png"
-  },
-  {
-    "revision": "5003a8c083f6a6aedada0865898c6194",
-    "url": "/img/projects/events/event-9-5.png"
   },
   {
     "revision": "9116f32d39fd4dc91d3040e58c3cdec5",
@@ -476,12 +476,12 @@ self.__precacheManifest = [
     "url": "/img/projects/prototype/prototype-6.png"
   },
   {
-    "revision": "3a857a44d95473bb4f6e1414ec31911c",
-    "url": "/img/projects/prototype/prototype-8-1.png"
-  },
-  {
     "revision": "9bca787bcf91ebdcb7976d8de2e62f14",
     "url": "/img/projects/prototype/prototype-7.png"
+  },
+  {
+    "revision": "3a857a44d95473bb4f6e1414ec31911c",
+    "url": "/img/projects/prototype/prototype-8-1.png"
   },
   {
     "revision": "054e6423db82a379fbc82b99657575a5",
@@ -496,15 +496,15 @@ self.__precacheManifest = [
     "url": "/img/projects/results/result-1-1.jpg"
   },
   {
+    "revision": "2a5d4aff1edfba03f15f3a57ac0aa395",
+    "url": "/img/projects/results/result-1-2.png"
+  },
+  {
     "revision": "b8d808a40abb8a1191d06989b5aaf9cd",
     "url": "/mstile-150x150.png"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/robots.txt"
-  },
-  {
-    "revision": "2a5d4aff1edfba03f15f3a57ac0aa395",
-    "url": "/img/projects/results/result-1-2.png"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/precache-manifest.b03d99866f774efed97a457e1452eca6.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/precache-manifest.a0866952984d344103aa2ba5e67ad2b0.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 self.addEventListener('message', (e) => {
     if (!e.data) {
